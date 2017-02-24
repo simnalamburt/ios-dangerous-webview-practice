afc8a743ae2be8b81eacc7ffd494859a098c4c64
Hyeon Kim <simnalamburt@gmail.com>
2017-02-24 14:22:54 +0900

    Initial Commit

---
+++ ios-dangerous-webview-practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-dangerous-webview-practice
+//
+//  Created by 김지현 on 2017. 2. 24..
+//  Copyright © 2017년 hyeonme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-dangerous-webview-practice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ios-dangerous-webview-practice
+//
+//  Created by 김지현 on 2017. 2. 24..
+//  Copyright © 2017년 hyeonme. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
